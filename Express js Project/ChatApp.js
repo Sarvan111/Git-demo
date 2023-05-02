9ed8ef6614efcce3a3b8671b132e86a0653bdf97
Sarvan111 <sarvan84a@gmail.com>
2023-05-02 22:56:09 +0530

    Routing filter

--- Express js Project/ChatApp.js
+++ Express js Project/ChatApp.js
@@ -1,17 +1,14 @@
 const express =require('express');
 const app=express();
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 app.use(express.urlencoded({
-    extended:true
+extended:true
 }));
 app.use(express.json());
-app.use('/add-product',(req,res,next)=>{
-res.send(`<form action="/product" method="POST"><input type="number" name="size">Product-Size<input type="text" name="title">Product-Name<button>Add Product</button></form>`) 
-});
-app.post('/product',(req,res,next)=>{
-console.log(req.body);
-res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-res.send("<h1>Hello from Express js</h1>");
+app.use("/admin",adminRoutes);
+app.use("/shop",shopRoutes);
+app.use((req,res,next)=>{
+res.status(404).send("<h1>Page not found</h1>");
 });
 app.listen(3000);
