bbbddb99230de06803f602df6885c3931077d466
Sarvan111 <sarvan84a@gmail.com>
2023-05-02 12:35:17 +0530

    Middleware  Project

--- Express js Project/ChatApp.js
+++ Express js Project/ChatApp.js
@@ -1,11 +1,17 @@
 const express =require('express');
 const app=express();
-app.use((req,res,next)=>{
-console.log("In the middleware"); 
-next();   
-})
-app.use((req,res,next)=>{
-console.log("In another middleware"); 
-res.send(`<h1>Hello form Express js!</h1>`);
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+app.use('/add-product',(req,res,next)=>{
+res.send(`<form action="/product" method="POST"><input type="number" name="size">Product-Size<input type="text" name="title">Product-Name<button>Add Product</button></form>`) 
 });
-app.listen(3000)
+app.post('/product',(req,res,next)=>{
+console.log(req.body);
+res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+res.send("<h1>Hello from Express js</h1>");
+});
+app.listen(3000);
